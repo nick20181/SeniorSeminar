424420913c582c2b242c3bae216d77a44db86c6c
Nick <nick2018191@gmail.com>
2019-10-28 19:37:59 -0500

    s

--- Campus.Custodial/Campus.Custodial.Chemicals/Controllers/ChemicalController.cs
+++ Campus.Custodial/Campus.Custodial.Chemicals/Controllers/ChemicalController.cs
@@ -33,8 +33,8 @@ namespace Campus.Custodial.Chemicals.Controllers
         {
             //Console.WriteLine($"MongoDB connection address: ");
             //mongoConnectionip = Console.ReadLine();
-            //datebase = new MongoDatabase($"mongodb://127.0.0.1:27017");
-            datebase = new MongoDatabase($"mongodb://10.100.128.135:27017");
+            datebase = new MongoDatabase($"mongodb://127.0.0.1:27017");
+            //datebase = new MongoDatabase($"mongodb://10.100.128.135:27017");
             var x = (MongoDatabase) datebase;
             Console.WriteLine($"Mongo connected at {x.GetMongoConnection()}");
             ChemFactory = new ChemicalFactory(datebase);
