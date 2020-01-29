e1fb21a140c651ef59e8a07f9adae2abc5eeb8bb
nick <nick2018191@gmail.com>
2020-01-29 15:42:10 -0600

    Update File Struc

--- Custodial.Addressing.Service/Custodial.Addressing.Service/Interfaces/IDatabaseObjectFactory.cs
+++ Custodial.Addressing.Service/Custodial.Addressing.Service/Interfaces/IDatabaseObjectFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Custodial.Addressing.Service
+namespace Custodial.Addressing.Service.Interfaces
 {
     public interface IDatabaseObjectFactory
     {
