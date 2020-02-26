cb46fd92e905a5e238b4f2025cb9de79e6de1f7d
nick <nick2018191@gmail.com>
2020-02-25 22:14:28 -0600

    Working with vue

---
+++ vue/nick-is-dumb/custodialservices/src/components/Test.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="test">
+    <ul v-if="posts && posts.length">
+    <li v-for="post of posts">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+  </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors">
+      {{error.message}}
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import axios from 'axios'
+
+@Component
+export default class Test extends Vue {
+  @Prop() msg;
+  data () {
+    return {
+      posts: [],
+      errors: []
+    }
+  }
+
+  mounted () {
+    axios.get('http://localhost:5000/Addressing/all')
+      .then(response => {
+      // JSON responses are automatically parsed.
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
